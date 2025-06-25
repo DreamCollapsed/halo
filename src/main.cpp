4e37e259c8714cfe487ddf3474e057f463142846
DreamCollapsed <weihongkai.i@qq.com>
2025-06-25 22:14:51 +0800

    fix folly build

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 
 int main() {
-    // 使用folly::fbstring
+    // Use folly::fbstring
     folly::fbstring hello = "Hello, ";
     folly::fbstring world = "World!";
     folly::fbstring message = hello + world;
 
-    // 使用folly::format
+    // Use folly::format
     std::cout << folly::format("Message: {}", message).str() << std::endl;
 
     return 0;

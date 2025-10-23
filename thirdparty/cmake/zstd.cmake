b69f396395312bdefbd619a0cae571146b943ed8
Hongkai Wei <hongkai.wei@qq.com>
2025-10-23 20:29:16 +0800

    debug linux ci zstd

--- thirdparty/cmake/zstd.cmake
+++ thirdparty/cmake/zstd.cmake
@@ -18,3 +18,13 @@ thirdparty_build_cmake_library("zstd"
 )
 
 find_package(zstd CONFIG REQUIRED)
+
+set(ZSTD_ROOT "${THIRDPARTY_INSTALL_DIR}/zstd" CACHE PATH "Root of halo-provided zstd" FORCE)
+set(ZSTD_INCLUDE_DIR "${THIRDPARTY_INSTALL_DIR}/zstd/include" CACHE PATH "Include dir for halo-provided zstd" FORCE)
+set(ZSTD_LIBRARY "${THIRDPARTY_INSTALL_DIR}/zstd/lib/libzstd.a" CACHE FILEPATH "Static zstd library for halo" FORCE)
+set(ZSTD_LIBRARY_RELEASE "${THIRDPARTY_INSTALL_DIR}/zstd/lib/libzstd.a" CACHE FILEPATH "Release zstd library for halo" FORCE)
+if(NOT DEFINED ZSTD_LIBRARY_DEBUG OR ZSTD_LIBRARY_DEBUG STREQUAL "ZSTD_LIBRARY_DEBUG-NOTFOUND")
+    set(ZSTD_LIBRARY_DEBUG "${THIRDPARTY_INSTALL_DIR}/zstd/lib/libzstd.a" CACHE FILEPATH "Debug zstd library (static)" FORCE)
+endif()
+
+message(STATUS "[halo:zstd] Using zstd static library at ${ZSTD_LIBRARY}")
